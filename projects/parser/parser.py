b1a90412bcc11772e8daab2966dc5eb9253bed6c
kais-radwan <kais.radwan.personal@gmail.com>
2024-11-29 22:19:25 +0200

    NLP project

---
+++ projects/parser/parser.py
@@ -0,0 +1,103 @@
+import nltk
+import sys
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+S -> NP VP | NP VP Conj NP VP | NP VP Conj VP
+NP -> N | Det N | Det AP N | P NP | NP P NP
+VP -> V | Adv VP | V Adv | VP NP | V NP Adv
+AP -> Adj | AP Adj
+"""
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def main():
+
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        s = input("Sentence: ")
+
+    # Convert input into list of words
+    s = preprocess(s)
+    print("sentence:", s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+    if not trees:
+        print("Could not parse sentence.")
+        return
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+
+def preprocess(sentence):
+    """
+    Convert `sentence` to a list of its words.
+    Pre-process sentence by converting all characters to lowercase
+    and removing any word that does not contain at least one alphabetic
+    character.
+    """
+    tokens = []
+
+    for word in str.split(sentence, " "):
+        word = str.lower(word)
+        valid = 0
+
+        for letter in word:
+            if letter.isalpha():
+                valid = 1
+                break
+
+        if valid == 0:
+            continue
+
+        tokens.append(nltk.word_tokenize(word)[0])
+
+    return tokens
+
+
+def np_chunk(tree):
+    chunks = []
+
+    for subtree in tree.subtrees():
+        if subtree.label() == "NP":
+            print(f"Found an NP: {subtree}")
+            if not any(child.label() == "NP" for child in subtree):
+                print(f"No nested NPs found, adding to chunks: {subtree}")
+                chunks.append(subtree)
+            else:
+                print(f"Nested NP found, skipping: {subtree}")
+
+    return chunks
+
+if __name__ == "__main__":
+    main()
